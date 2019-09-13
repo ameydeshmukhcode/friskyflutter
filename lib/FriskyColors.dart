58a95a28572299f65e4f01ddbf8526750e7a2751
raj vaya <rajkumarvaya@gmail.com>
2019-09-13 16:32:59 +0530

    Login nd Color Class init

---
+++ lib/FriskyColors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class FriskyColor
+{
+
+  Color white = Colors.white;
+
+
+}
---
+++ lib/Login/UserLogin.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:friskyflutter/FriskyColors.dart';
+class UserLogin extends StatefulWidget {
+  @override
+  _UserLoginState createState() => _UserLoginState();
+}
+
+class _UserLoginState extends State<UserLogin> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: FriskyColor().white,
+      body: Container(
+
+
+      ),
+    );
+  }
+}
