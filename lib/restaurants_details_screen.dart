58ea756c8aa8c428e9925b89a53f55c64991f709
Amey Deshmukh <amey23deshmukh@gmail.com>
2019-10-20 13:22:47 +0530

    Removed unused import

--- lib/restaurants_details_screen.dart
+++ lib/restaurants_details_screen.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:friskyflutter/size_config.dart';
 
 class DetailsPage extends StatefulWidget {
