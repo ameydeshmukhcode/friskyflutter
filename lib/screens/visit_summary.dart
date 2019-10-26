75331a14b1f530bcf21c3af6a93b3c9b7f566ab8
Amey Deshmukh <amey23deshmukh@gmail.com>
2019-10-26 17:21:41 +0530

    Added widgets to show order summary extras

--- lib/screens/visit_summary.dart
+++ lib/screens/visit_summary.dart
@@ -1,5 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:date_format/date_format.dart';
 import 'package:flutter/material.dart';
+import 'package:friskyflutter/structures/order_item.dart';
+import 'package:friskyflutter/structures/order_status.dart';
 
 class VisitSummary extends StatefulWidget {
   final String sessionID;
@@ -18,6 +21,10 @@ class VisitSummary extends StatefulWidget {
 }
 
 class _VisitSummaryState extends State<VisitSummary> {
+  bool isLoading = true;
+
+  List<OrderItem> _orderItems = List<OrderItem>();
+  Future _orderItemsList;
   Timestamp endTime;
   String amountPayable;
   String gst;
@@ -25,35 +32,114 @@ class _VisitSummaryState extends State<VisitSummary> {
 
   @override
   void initState() {
-    super.initState();
-
     _getVisitSummary().whenComplete(() {
-      print(endTime.toString() +
-          " " +
-          amountPayable.toString() +
-          " " +
-          gst.toString() +
-          " " +
-          billAmount.toString());
-      _getOrders().whenComplete(() {});
+      _orderItemsList = _getOrders().whenComplete(() {
+        setState(() {
+          isLoading = false;
+        });
+      });
     });
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "Visit Summary",
-            style: TextStyle(
-              color: Colors.black,
+        backgroundColor: Colors.white,
+        // body: //_visitsList(),
+        body: Center(
+          child: _orderSummary(),
+        ));
+  }
+
+  Widget _orderSummary() {
+    return FutureBuilder(
+      future: _orderItemsList,
+      builder: (context, snapshot) {
+        return Scaffold(
+            appBar: AppBar(
+              title: Text(
+                "Visit Summary",
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+              ),
+              elevation: 0,
+              backgroundColor: Colors.transparent,
+              iconTheme: IconThemeData(color: Colors.black),
             ),
-          ),
-          elevation: 0,
-          backgroundColor: Colors.transparent,
-          iconTheme: IconThemeData(color: Colors.black),
-        ),
-        body: Column());
+            body: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Text(
+                  widget.restaurantName,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 8),
+                ),
+                Text(
+                  formatDate(endTime.toDate(), [dd, ' ', M, ' ', yyyy,' ', hh,':', nn, ' ', am]),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 14,
+                  ),
+                ),
+                Divider(
+                  indent: 8,
+                  endIndent: 8,
+                ),
+                //TODO Add order list here
+                Divider(
+                  indent: 8,
+                  endIndent: 8,
+                ),
+                Container(
+                  padding: EdgeInsets.only(left: 16, right: 16),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text("Order total"),
+                      Text("\u20B9 " + billAmount)
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(left: 16, right: 16),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[Text("GST"), Text("\u20B9 " + gst)],
+                  ),
+                ),
+                Divider(
+                  indent: 8,
+                  endIndent: 8,
+                ),
+                Container(
+                  padding: EdgeInsets.only(left: 16, right: 16),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        "Final Total",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      Text(
+                        "\u20B9 " + gst,
+                        style: TextStyle(fontSize: 20),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ));
+      },
+    );
   }
 
   Future _getVisitSummary() async {
@@ -80,8 +166,34 @@ class _VisitSummaryState extends State<VisitSummary> {
         .getDocuments()
         .then((data) async {
       for (DocumentSnapshot documentSnapshot in data.documents) {
-        print(documentSnapshot.data);
+        Map<dynamic, dynamic> items = documentSnapshot.data["items"];
+
+        for (int i = 0; i < items.length; i++) {
+          String itemID = items.keys.elementAt(i).toString();
+          Map<dynamic, dynamic> itemData = items.values.elementAt(i);
+          String name = itemData["name"];
+          int cost = int.parse(itemData["cost"]);
+          int quantity = itemData["quantity"];
+
+          OrderItem orderItem =
+              OrderItem(itemID, name, quantity, (quantity * cost));
+
+          if (itemData["status"].toString().compareTo("pending") == 0) {
+            orderItem.orderStatus = OrderStatus.pending;
+          } else if (itemData["status"].toString().compareTo("accepted") == 0) {
+            orderItem.orderStatus = OrderStatus.accepted;
+          } else if (itemData["status"].toString().compareTo("rejected") == 0) {
+            orderItem.orderStatus = OrderStatus.rejected;
+          } else if (itemData["status"].toString().compareTo("cancelled") ==
+              0) {
+            orderItem.orderStatus = OrderStatus.cancelled;
+          }
+
+          _orderItems.add(orderItem);
+        }
       }
     });
+
+    return _orderItemsList;
   }
 }
