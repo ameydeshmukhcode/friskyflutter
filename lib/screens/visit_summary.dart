96f7eb8ce66703cf78a2f3dcdcde5abf89decd17
Amey Deshmukh <amey23deshmukh@gmail.com>
2019-10-26 16:15:21 +0530

    Added methods to retrieve visit summary and orders

--- lib/screens/visit_summary.dart
+++ lib/screens/visit_summary.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class VisitSummary extends StatefulWidget {
@@ -17,31 +18,70 @@ class VisitSummary extends StatefulWidget {
 }
 
 class _VisitSummaryState extends State<VisitSummary> {
+  Timestamp endTime;
+  String amountPayable;
+  String gst;
+  String billAmount;
+
   @override
   void initState() {
     super.initState();
+
+    _getVisitSummary().whenComplete(() {
+      print(endTime.toString() +
+          " " +
+          amountPayable.toString() +
+          " " +
+          gst.toString() +
+          " " +
+          billAmount.toString());
+      _getOrders().whenComplete(() {});
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-
-    print(widget.sessionID + " " + widget.restaurantID + " " + widget.restaurantName);
-
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Visit Summary",
-          style: TextStyle(
-            color: Colors.black,
+        appBar: AppBar(
+          title: Text(
+            "Visit Summary",
+            style: TextStyle(
+              color: Colors.black,
+            ),
           ),
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          iconTheme: IconThemeData(color: Colors.black),
         ),
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-        iconTheme: IconThemeData(color: Colors.black),
-      ),
-      body: Center(
-        child: Container(),
-      ),
-    );
+        body: Column());
+  }
+
+  Future _getVisitSummary() async {
+    await Firestore.instance
+        .collection("restaurants")
+        .document(widget.restaurantID)
+        .collection("sessions")
+        .document(widget.sessionID)
+        .get()
+        .then((data) async {
+      endTime = data["end_time"];
+      amountPayable = data["amount_payable"];
+      gst = data["gst"];
+      billAmount = data["bill_amount"];
+    });
+  }
+
+  Future _getOrders() async {
+    await Firestore.instance
+        .collection("restaurants")
+        .document(widget.restaurantID)
+        .collection("orders")
+        .where("session_id", isEqualTo: widget.sessionID)
+        .getDocuments()
+        .then((data) async {
+      for (DocumentSnapshot documentSnapshot in data.documents) {
+        print(documentSnapshot.data);
+      }
+    });
   }
 }
