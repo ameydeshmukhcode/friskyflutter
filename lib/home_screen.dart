459c7075bd397cc6e2bfeff8335be9b717215930
raj vaya <rajkumarvaya@gmail.com>
2019-11-01 12:47:54 +0530

    PageView Change TO TabBarView

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -15,11 +15,18 @@ class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
-  PageController _pageController;
-  int _page = 0;
-  //bool session=false;
-  // SharedPreferences sharedPreferences;
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  int currentIndex = 0;
+
+  TabController _tabController;
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
@@ -29,33 +36,20 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
     super.initState();
-    //getSession();
-    //Provider.of<Session>(context).getStatus();
-    _pageController = new PageController();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    _pageController.dispose();
-  }
-
-  void navigationTapped(int page) {
-    _pageController.animateToPage(page,
-        duration: const Duration(milliseconds: 300),
-        curve: Curves.fastOutSlowIn);
-  }
-
-  void onPageChanged(int page) {
-    setState(() {
-      this._page = page;
-    });
+    _tabController = TabController(length: 3, vsync: this);
   }
 
   Widget _bottomNavBar() {
     return BottomNavigationBar(
-      onTap: navigationTapped,
-      currentIndex: _page,
+      onTap: (index) {
+        setState(
+          () {
+            currentIndex = index;
+            _tabController.animateTo(currentIndex, curve: Curves.easeOutBack);
+          },
+        );
+      },
+      currentIndex: currentIndex,
       showUnselectedLabels: false,
       items: <BottomNavigationBarItem>[
         BottomNavigationBarItem(
@@ -96,74 +90,83 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Consumer<Session>(
-        // ignore: non_constant_identifier_names
+          // ignore: non_constant_identifier_names
           builder: (context, Session, child) {
-            return PageView(
-              children: <Widget>[
-                HomeTab(),
-                Session.isSessionActive ? DineOrders() : DineTab(),
-                VisitTab(),
-              ],
-              onPageChanged: onPageChanged,
-              controller: _pageController,
-            );
-          }),
+        return TabBarView(
+          children: [
+            HomeTab(),
+            Session.isSessionActive ? DineOrders() : DineTab(),
+            VisitTab(),
+          ],
+          controller: _tabController,
+          physics: NeverScrollableScrollPhysics(),
+        );
+      }),
       bottomSheet: Consumer<Session>(
         builder: (context, session, child) {
-          return Visibility(
-              visible: session.isSessionActive,
-              child: ListTile(
-                title: Text(
-                  "Currently at",
-                  style: TextStyle(
-                      fontSize: 14, color: FriskyColor().colorTextLight),
-                ),
-                subtitle: Text(
-                    session.restaurantName + " - Table " + session.tableName,
-                    style:
-                    TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-                trailing: OutlineButton(
-                  color: Colors.lightGreen,
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => MenuScreen(
-                                session.restaurantName,
-                                session.tableName,
-                                session.tableName,
-                                session.restaurantName)));
-                  },
-                  child: Text(
-                    "Menu",
-                    style: TextStyle(color: FriskyColor().colorPrimary),
-                  ),
-                  borderSide: BorderSide(
-                    color: FriskyColor().colorPrimary,
-                    width: 1.5,
-                  ),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: new BorderRadius.circular(4.0),
-                  ),
-                ),
-              ));
+//          return
+          return BottomSheet(
+              onClosing: () {},
+              builder: (BuildContext context) {
+                return Visibility(
+                    visible: session.isSessionActive,
+                    child: ListTile(
+                      title: Text(
+                        "Currently at",
+                        style: TextStyle(
+                            fontSize: 14, color: FriskyColor().colorTextLight),
+                      ),
+                      subtitle: Text(
+                          session.restaurantName +
+                              " - Table " +
+                              session.tableName,
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold)),
+                      trailing: OutlineButton(
+                        color: Colors.lightGreen,
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => MenuScreen(
+                                      session.restaurantName,
+                                      session.tableName,
+                                      session.sessionID,
+                                      session.restaurantID)));
+                        },
+                        child: Text(
+                          "Menu",
+                          style: TextStyle(color: FriskyColor().colorPrimary),
+                        ),
+                        borderSide: BorderSide(
+                          color: FriskyColor().colorPrimary,
+                          width: 1.5,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: new BorderRadius.circular(4.0),
+                        ),
+                      ),
+                    ));
+              },
+              enableDrag: true,
+              );
         },
       ),
       floatingActionButton: Consumer<Session>(
-        // ignore: non_constant_identifier_names
+          // ignore: non_constant_identifier_names
           builder: (context, Session, child) {
-            return Visibility(
-              visible: !Session.isSessionActive,
-              child: FloatingActionButton.extended(
-                onPressed: () {
-                  Navigator.pushNamed(context, "/scan");
-                },
-                icon: Icon(MdiIcons.qrcode),
-                label: Text("Scan QR Code"),
-                backgroundColor: FriskyColor().colorPrimary,
-              ),
-            );
-          }),
+        return Visibility(
+          visible: !Session.isSessionActive,
+          child: FloatingActionButton.extended(
+            onPressed: () {
+              Navigator.pushNamed(context, "/scan");
+            },
+            icon: Icon(MdiIcons.qrcode),
+            label: Text("Scan QR Code"),
+            backgroundColor: FriskyColor().colorPrimary,
+          ),
+        );
+      }),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       bottomNavigationBar: _bottomNavBar(),
     );
