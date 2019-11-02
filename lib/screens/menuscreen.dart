6e59b437967f53a7706ea6a9cd2586554b4f1e34
raj vaya <rajkumarvaya@gmail.com>
2019-11-03 00:53:59 +0530

    MenuScreen Without Shimmer Effect

--- lib/screens/menuscreen.dart
+++ lib/screens/menuscreen.dart
@@ -1,6 +1,5 @@
 import 'package:friskyflutter/size_config.dart';
 import 'dart:collection';
-import 'package:shimmer/shimmer.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:friskyflutter/structures/MenuCategory.dart';
