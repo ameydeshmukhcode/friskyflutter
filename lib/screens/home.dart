5d448028a2faa429744ea570c2d051b78278a128
Amey Deshmukh <amey23deshmukh@gmail.com>
2019-11-04 20:40:35 +0530

    Removed unused methods and imports

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,13 +2,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:friskyflutter/login/user_login.dart';
 import 'package:friskyflutter/screens/options.dart';
 import 'package:friskyflutter/size_config.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import '../frisky_colors.dart';
-import 'package:provider/provider.dart';
-import 'package:friskyflutter/provider_models/session.dart';
 import '../restaurants_details_screen.dart';
 
 class HomeTab extends StatefulWidget {
@@ -32,13 +28,6 @@ class _HomeTabState extends State<HomeTab>
             builder: (context) => DetailsPage(
                   resturant: restaurant,
                 )));
-  }
-
-  signOut() async {
-    _auth.signOut();
-    Navigator.pushReplacement(
-        context, new MaterialPageRoute(
-        builder: (context) => UserLogin()));
   }
 
   getUser() async {
