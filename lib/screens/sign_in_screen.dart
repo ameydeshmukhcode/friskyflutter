1d18213d0af4a675043c67f997795d9d5b440614
Amey Deshmukh <amey23deshmukh@gmail.com>
2019-11-16 13:35:14 +0530

    Fixed up sign in screen

--- lib/screens/sign_in_screen.dart
+++ lib/screens/sign_in_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:friskyflutter/frisky_colors.dart';
 import 'package:friskyflutter/screens/sign_in_email.dart';
-import 'package:friskyflutter/size_config.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class UserLogin extends StatefulWidget {
@@ -15,38 +14,131 @@ class _UserLoginState extends State<UserLogin> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final GoogleSignIn _googleSignIn = GoogleSignIn();
 
-  showLoader() {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: <Widget>[
+          Expanded(
+              child: Padding(
+            padding: EdgeInsets.only(left: 64, right: 64),
+            child: SvgPicture.asset(
+              'images/logo_text.svg',
+            ),
+          )),
+          Container(
+            padding: EdgeInsets.fromLTRB(24, 0, 24, 0),
+            child: RaisedButton(
+              elevation: 2,
+              color: Colors.white,
+              padding: EdgeInsets.all(8),
+              shape: StadiumBorder(),
+              onPressed: _signInWithGoogle,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  SvgPicture.asset(
+                    'images/icons/google.svg',
+                    height: 20,
+                    width: 20,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(4),
+                  ),
+                  Text("Use Google Account",
+                      style: TextStyle(
+                        fontFamily: "museoM",
+                        fontSize: 20,
+                        color: FriskyColor().colorPrimary,
+                      )),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(24, 4, 24, 0),
+            child: RaisedButton(
+              color: FriskyColor().colorPrimary,
+              padding: EdgeInsets.all(8),
+              shape: StadiumBorder(),
+              elevation: 2,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(
+                    Icons.mail,
+                    color: Colors.white,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(4),
+                  ),
+                  Text("Use Email Address",
+                      style: TextStyle(
+                          fontFamily: "museoM",
+                          fontSize: 20,
+                          color: Colors.white)),
+                ],
+              ),
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return EmailSignIn();
+                    },
+                  ),
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(56, 8, 56, 8),
+            child: Text(
+              "By signing in, you\'re agreeing to our Terms and Privacy Policy",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontFamily: "museoS",
+                fontSize: 14,
+                color: FriskyColor().colorTextDark,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  _showProgressDialog() {
     showDialog(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text(
-            "Signing you in",
-            style: TextStyle(
-                fontFamily: "museo",
-                fontWeight: FontWeight.w100,
-                color: FriskyColor().colorTextDark),
-          ),
-          content: Container(
-            height: SizeConfig.safeBlockVertical * 10,
-            width: SizeConfig.safeBlockVertical * 10,
-            child: Center(
-              child: CircularProgressIndicator(
-                valueColor: new AlwaysStoppedAnimation<Color>(
-                  FriskyColor().colorPrimary,
-                ),
-              ),
+            title: Text(
+              "Signing you in",
+              style: TextStyle(
+                  fontFamily: "museoL", color: FriskyColor().colorTextDark),
             ),
-          ),
-        );
+            content: Wrap(
+              children: <Widget>[
+                Center(
+                  child: CircularProgressIndicator(
+                    valueColor: new AlwaysStoppedAnimation<Color>(
+                      FriskyColor().colorPrimary,
+                    ),
+                  ),
+                )
+              ],
+            ));
       },
       barrierDismissible: false,
     );
   }
 
-  googleSignIn() async {
+  _signInWithGoogle() async {
     try {
-      showLoader();
+      _showProgressDialog();
       final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
       final GoogleSignInAuthentication googleAuth =
           await googleUser.authentication;
@@ -62,132 +154,6 @@ class _UserLoginState extends State<UserLogin> {
       return user;
     } catch (e) {
       Navigator.pop(context);
-      showError(e.message);
     }
-  }
-
-  showError(String errorMessage) {
-    showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text('Error'),
-            content: Text(errorMessage),
-            actions: <Widget>[
-              FlatButton(
-                child: Text('OK'),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              )
-            ],
-          );
-        });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    SizeConfig().init(context);
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Container(
-        height: SizeConfig.screenHeight,
-        width: SizeConfig.screenWidth,
-        child: Center(
-          child: Column(
-            children: <Widget>[
-              Expanded(
-                child: SvgPicture.asset(
-                  'images/logo_text.svg',
-                  height: SizeConfig.safeBlockVertical * 14,
-                  width: SizeConfig.safeBlockHorizontal * 56,
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.fromLTRB(24, 0, 24, 0),
-                child: RaisedButton(
-                  padding: EdgeInsets.all(10),
-                  shape: StadiumBorder(),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      SvgPicture.asset(
-                        'images/icons/google.svg',
-                        height: 20,
-                        width: 20,
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(8),
-                      ),
-                      Text("Use Google Account",
-                          style: TextStyle(
-                            fontFamily: "museo",
-                            fontWeight: FontWeight.w500,
-                            fontSize: 20,
-                            color: FriskyColor().colorPrimary,
-                          )),
-                    ],
-                  ),
-                  onPressed: googleSignIn,
-                  elevation: 2,
-                  color: Colors.white,
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.fromLTRB(24, 8, 24, 0),
-                child: RaisedButton(
-                  padding: EdgeInsets.all(10),
-                  shape: StadiumBorder(),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Icon(
-                        Icons.mail,
-                        color: Colors.white,
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(8),
-                      ),
-                      Text("Use Email Address",
-                          style: TextStyle(
-                              fontFamily: "museo",
-                              fontWeight: FontWeight.w500,
-                              fontSize: 20,
-                              color: Colors.white)),
-                    ],
-                  ),
-                  onPressed: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) {
-                          return EmailSignIn();
-                        },
-                      ),
-                    );
-                  },
-                  elevation: 2,
-                  color: FriskyColor().colorPrimary,
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.fromLTRB(56, 8, 56, 8),
-                child: Text(
-                  "By signing in, you\'re agreeing to our Terms and Privacy Policy",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontFamily: "museo",
-                    fontWeight: FontWeight.w100,
-                    fontSize: 14,
-                    color: FriskyColor().colorTextDark,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
   }
 }
