6bbe179f27d9833d2cfc54745b93a981c34bbd71
Amey Deshmukh <amey23deshmukh@gmail.com>
2019-11-26 00:10:22 +0530

    Added dummy buttons to slideshow

--- lib/screens/slideshow_screen.dart
+++ lib/screens/slideshow_screen.dart
@@ -2,25 +2,41 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/svg.dart';
 
-import '../frisky_colors.dart';
-
 class SlideshowScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return PageView(
+    return Stack(
       children: <Widget>[
-        Slide(
-            "Scan QR code",
-            "Scan QR codes on tables at any restaurant to start your dining experience.",
-            'images/state_graphics/slide_scan.svg'),
-        Slide(
-            "Instant Menu",
-            "Access the menu and browse your favorite dishes instantly.\nPlace orders without having to wait.",
-            'images/state_graphics/slide_menu.svg'),
-        Slide(
-            "Keep Tabs",
-            "Get real-time bill and order updates and check your previous orders at anytime.",
-            'images/state_graphics/slide_summary.svg')
+        PageView(
+          children: <Widget>[
+            Slide(
+                "Scan QR code",
+                "Scan QR codes on tables at any restaurant to start your dining experience.",
+                'images/state_graphics/slide_scan.svg'),
+            Slide(
+                "Instant Menu",
+                "Access the menu and browse your favorite dishes instantly.\nPlace orders without having to wait.",
+                'images/state_graphics/slide_menu.svg'),
+            Slide(
+                "Keep Tabs",
+                "Get real-time bill and order updates and check your previous orders at anytime.",
+                'images/state_graphics/slide_summary.svg')
+          ],
+        ),
+        Positioned(
+          left: 24,
+          bottom: 16,
+          child: FlatButton(
+            child: Text("Skip"),
+          ),
+        ),
+        Positioned(
+          right: 24,
+          bottom: 16,
+          child: FlatButton(
+            child: Text("Next"),
+          ),
+        ),
       ],
     );
   }
