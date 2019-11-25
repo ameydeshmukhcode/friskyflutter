64e0fe23d29baa187ff06dbbb7ee333a805ac8a5
Amey Deshmukh <amey23deshmukh@gmail.com>
2019-11-26 00:01:04 +0530

    Added default slides to app launch slideshow

--- lib/screens/slideshow_screen.dart
+++ lib/screens/slideshow_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/svg.dart';
 
+import '../frisky_colors.dart';
+
 class SlideshowScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -10,15 +12,15 @@ class SlideshowScreen extends StatelessWidget {
         Slide(
             "Scan QR code",
             "Scan QR codes on tables at any restaurant to start your dining experience.",
-            'images/state_graphics/state_scan_qr.svg'),
+            'images/state_graphics/slide_scan.svg'),
         Slide(
             "Instant Menu",
             "Access the menu and browse your favorite dishes instantly.\nPlace orders without having to wait.",
-            'images/state_graphics/state_scan_qr.svg'),
+            'images/state_graphics/slide_menu.svg'),
         Slide(
             "Keep Tabs",
             "Get real-time bill and order updates and check your previous orders at anytime.",
-            'images/state_graphics/state_scan_qr.svg')
+            'images/state_graphics/slide_summary.svg')
       ],
     );
   }
@@ -31,21 +33,32 @@ class Slide extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Padding(
-      padding: EdgeInsets.only(left: 24, right: 24),
-      child: Column(
-        children: <Widget>[
-          AspectRatio(
-            aspectRatio: 1,
-            child: SvgPicture.asset(
-              imagePath,
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        padding: EdgeInsets.only(left: 24, right: 24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 1,
+              child: SvgPicture.asset(imagePath),
             ),
-          ),
-          Text(title),
-          Text(description)
-        ],
+            Padding(
+              padding: EdgeInsets.only(top: 4, bottom: 4),
+              child: Text(
+                title,
+                style: TextStyle(fontFamily: "museoM", fontSize: 20),
+              ),
+            ),
+            Text(
+              description,
+              style: TextStyle(fontFamily: "museoS", fontSize: 18),
+            )
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
