06b7283fad0cdf66cbe36dd8853e7f259a05528c
Amey Deshmukh <amey23deshmukh@gmail.com>
2019-11-26 22:43:26 +0530

    Updated to use VisitWidget in visits tab

--- lib/screens/visits_tab.dart
+++ lib/screens/visits_tab.dart
@@ -1,14 +1,13 @@
 import 'dart:core';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:date_format/date_format.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:friskyflutter/structures/visit.dart';
+import 'package:friskyflutter/widgets/card_visit.dart';
 
 import '../frisky_colors.dart';
-import 'visit_summary.dart';
 
 // ignore: non_constant_identifier_names
 List<Visit> VisitsList = List<Visit>();
@@ -148,117 +147,18 @@ class _VisitsTabState extends State<VisitsTab>
                   itemCount: VisitsList.length,
                   scrollDirection: Axis.vertical,
                   itemBuilder: (context, index) {
-                    return Container(
-                        height: 100,
-                        padding: const EdgeInsets.fromLTRB(4, 4, 4, 4),
-                        child: Card(
-                          margin: EdgeInsets.all(0),
-                          elevation: 2,
-                          child: InkWell(
-                            onTap: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => VisitSummary(
-                                          sessionID:
-                                              VisitsList[index].sessionID,
-                                          restaurantID:
-                                              VisitsList[index].restaurantID,
-                                          restaurantName:
-                                              VisitsList[index].restaurantName,
-                                        )),
-                              );
-                            },
-                            child: Row(
-                              children: <Widget>[
-                                Expanded(
-                                  child: Image.network(
-                                      VisitsList[index].restaurantImage,
-                                      fit: BoxFit.cover),
-                                ),
-                                Expanded(
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(4),
-                                    child: Column(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.start,
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: <Widget>[
-                                        Text(
-                                          VisitsList[index].restaurantName,
-                                          maxLines: 1,
-                                          overflow: TextOverflow.ellipsis,
-                                          softWrap: true,
-                                          style: TextStyle(
-                                              color:
-                                                  FriskyColor().colorTextLight,
-                                              fontSize: 14,
-                                              fontWeight: FontWeight.bold,
-                                              fontFamily: "museoM"),
-                                        ),
-                                        Padding(
-                                          padding: EdgeInsets.only(top: 4),
-                                        ),
-                                        Text(
-                                          "Visited On ",
-                                          maxLines: 1,
-                                          style: TextStyle(
-                                              color:
-                                                  FriskyColor().colorTextLight,
-                                              fontSize: 12,
-                                              fontFamily: "museoS"),
-                                        ),
-                                        Text(
-                                          _getFormattedTimestamp(
-                                              VisitsList[index].endTime),
-                                          maxLines: 1,
-                                          style: TextStyle(
-                                              fontSize: 12,
-                                              color:
-                                                  FriskyColor().colorTextLight,
-                                              fontWeight: FontWeight.bold,
-                                              fontFamily: "museoS"),
-                                        ),
-                                        Padding(
-                                          padding: EdgeInsets.only(top: 4),
-                                        ),
-                                        Text(
-                                          "Total Amount",
-                                          maxLines: 1,
-                                          style: TextStyle(
-                                              fontSize: 12,
-                                              color:
-                                                  FriskyColor().colorTextLight,
-                                              fontFamily: "museoS"),
-                                        ),
-                                        Text(
-                                          "\u20B9 " +
-                                              VisitsList[index].totalAmount,
-                                          style: TextStyle(
-                                              fontSize: 12,
-                                              color:
-                                                  FriskyColor().colorTextLight,
-                                              fontWeight: FontWeight.bold,
-                                              fontFamily: "museoS"),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                )
-                              ],
-                            ),
-                          ),
-                        ));
+                    return VisitWidget(
+                      VisitsList[index].sessionID,
+                      VisitsList[index].restaurantID,
+                      VisitsList[index].restaurantName,
+                      VisitsList[index].restaurantImage,
+                      VisitsList[index].endTime,
+                      VisitsList[index].totalAmount,
+                    );
                   });
             }
           }
         });
-  }
-
-  _getFormattedTimestamp(Timestamp timestamp) {
-    return formatDate(
-        timestamp.toDate(), [dd, ' ', M, ' ', yyyy, ' ', hh, ':', nn, '', am]);
   }
 
   /*_shimmerLoader() {
