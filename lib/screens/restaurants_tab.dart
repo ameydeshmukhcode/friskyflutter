c7aa8a0064bdd8109103ce04e0c1d2d63228f672
Amey Deshmukh <amey23deshmukh@gmail.com>
2020-01-09 00:11:11 +0530

    Added new StatelessWidget for Restaurant Card

--- lib/screens/restaurants_tab.dart
+++ lib/screens/restaurants_tab.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:friskyflutter/widgets/bottom_sheet_restaurant.dart';
+import 'package:friskyflutter/widgets/card_restaurant.dart';
 
 import '../frisky_colors.dart';
 
@@ -97,90 +98,16 @@ class _RestaurantsTabState extends State<RestaurantsTab>
                 scrollDirection: Axis.vertical,
                 itemBuilder: (context, index) {
                   return InkWell(
-                    onTap: () {
-                      _showRestaurantDetails(snapshot.data[index]);
-                    },
-                    child: Container(
-                      padding: EdgeInsets.fromLTRB(8, 8, 8, 8),
-                      height: 120,
-                      child: Row(
-                        children: <Widget>[
-                          AspectRatio(
-                            aspectRatio: 1.25,
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(10),
-                              child: Image.network(
-                                snapshot.data[index].data['image'],
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                          ),
-                          Expanded(
-                              child: Container(
-                            child: Padding(
-                              padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
-                              child: Column(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: <Widget>[
-                                  Column(
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: <Widget>[
-                                      Text(
-                                        snapshot.data[index].data['name'],
-                                        maxLines: 1,
-                                        overflow: TextOverflow.ellipsis,
-                                        softWrap: true,
-                                        style: TextStyle(
-                                          fontSize: 18,
-                                          fontWeight: FontWeight.w700,
-                                        ),
-                                      ),
-                                      Text(
-                                        snapshot.data[index].data['address'],
-                                        maxLines: 1,
-                                        style: TextStyle(fontSize: 14),
-                                      ),
-                                      Text(
-                                        snapshot.data[index].data['cuisine']
-                                                [0] +
-                                            ", " +
-                                            snapshot.data[index].data['cuisine']
-                                                [1],
-                                        style: TextStyle(fontSize: 14),
-                                      ),
-                                    ],
-                                  ),
-                                  Align(
-                                    alignment: Alignment.bottomRight,
-                                    child: DecoratedBox(
-                                      decoration: BoxDecoration(
-                                          borderRadius:
-                                              BorderRadius.circular(4),
-                                          color: Colors.green),
-                                      child: Padding(
-                                        padding: EdgeInsets.all(4),
-                                        child: Text(
-                                          "4.5",
-                                          style: TextStyle(
-                                              color: Colors.white,
-                                              fontSize: 18,
-                                              fontWeight: FontWeight.w700),
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ))
-                        ],
-                      ),
-                    ),
-                  );
+                      onTap: () {
+                        _showRestaurantDetails(snapshot.data[index]);
+                      },
+                      child: RestaurantCard(
+                          snapshot.data[index].data['image'],
+                          snapshot.data[index].data['name'],
+                          snapshot.data[index].data['address'],
+                          snapshot.data[index].data['cuisine'][0] +
+                              ", " +
+                              snapshot.data[index].data['cuisine'][1]));
                 });
           }
         });
---
+++ lib/widgets/card_restaurant.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class RestaurantCard extends StatelessWidget {
+  final String image, name, address, cuisine;
+
+  RestaurantCard(this.image, this.name, this.address, this.cuisine);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(8, 8, 8, 8),
+      height: 120,
+      child: Row(
+        children: <Widget>[
+          AspectRatio(
+            aspectRatio: 1.25,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Image.network(
+                image,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Expanded(
+              child: Container(
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        name,
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        softWrap: true,
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      Text(
+                        address,
+                        maxLines: 1,
+                        style: TextStyle(fontSize: 14),
+                      ),
+                      Text(
+                        cuisine,
+                        style: TextStyle(fontSize: 14),
+                      ),
+                    ],
+                  ),
+                  Align(
+                    alignment: Alignment.bottomRight,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(4),
+                          color: Colors.green),
+                      child: Padding(
+                        padding: EdgeInsets.all(4),
+                        child: Text(
+                          "4.5",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w700),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ))
+        ],
+      ),
+    );
+  }
+}
