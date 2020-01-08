dbd09b4bf41eff76059e9c9e267c1aa51e4438a3
Amey Deshmukh <amey23deshmukh@gmail.com>
2020-01-08 23:42:36 +0530

    Set default app theme canvas color to transparent

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class MyApp extends StatelessWidget {
             theme: ThemeData(
               primaryColor: FriskyColor.colorPrimary,
               accentColor: FriskyColor.colorPrimary,
+              canvasColor: Colors.transparent
             ),
             home: InitWidget()));
   }
