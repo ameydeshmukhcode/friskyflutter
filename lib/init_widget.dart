4ae962ce4a4a0a8268648764fbae5e67bb8b89e9
Amey Deshmukh <amey23deshmukh@gmail.com>
2020-03-01 14:49:18 +0530

    Renamed icon

--- lib/init_widget.dart
+++ lib/init_widget.dart
@@ -31,7 +31,7 @@ class _InitWidgetState extends State<InitWidget> {
     super.initState();
     _fcmListeners();
 
-    var android = new AndroidInitializationSettings('logo');
+    var android = new AndroidInitializationSettings('icon_notif');
     var iOS = new IOSInitializationSettings();
     var initSettings = new InitializationSettings(android, iOS);
     _flutterLocalNotificationsPlugin.initialize(initSettings,
