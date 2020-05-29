83659ef1b4c7495a94fd20dbf9be5292e4e5907f
raj vaya <rajkumarvaya@gmail.com>
2020-05-29 18:38:58 +0530

    unused packages removed to fix Build

--- lib/icebreaker/screens/icebreaker_home.dart
+++ lib/icebreaker/screens/icebreaker_home.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:flutter_svg/parser.dart';
 import 'package:friskyflutter/icebreaker/screens/widgets/custom_switch.dart';
 
 class IceBreakerHome extends StatefulWidget {
